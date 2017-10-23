0b691853a9a92f2f051af09c3f9516c6c3f4ef6a
F・ノバルティス <Hiromi.Kaede@gmail.com>
2017-10-23 17:55:39 +0800

    sync

--- TempSync/VSCodeUserData/set_user_data.bat
+++ TempSync/VSCodeUserData/set_user_data.bat
@@ -10,6 +10,9 @@ mklink /j "%userprofile%\.vscode\extensions" .\extensions
 rd/s/q "%appdata%\Code - Insiders\User"
 rd/s/q "%userprofile%\.vscode-insiders\extensions"
 
+md "%appdata%\Code - Insiders" 2>NUL
+md "%userprofile%\.vscode-insiders" 2>NUL
+
 mklink /j "%appdata%\Code - Insiders\User" .
 mklink /j "%userprofile%\.vscode-insiders\extensions" .\extensions
 
